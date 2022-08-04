21d1aa4f072cdbc24aeba99dcf12707517197cbd
Manucho40 <rickypaine40@gmail.com>
2022-08-04 21:52:05 +0000

    Register 04/08/2022 AT HOME

--- front/src/app/store.ts
+++ front/src/app/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "../features/userSlice";
-
 export const store = configureStore({
     reducer: {
         users: userSlice.reducer,
-    }
+    },
+   
 })
 
 
