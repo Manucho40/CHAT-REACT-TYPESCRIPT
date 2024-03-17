6f61240e84cbe14d07a80a113f35e8c711c51310
Manucho40 <rickypine40@gmail.com>
2024-03-17 21:49:41 +0000

    update api url

--- front/src/config.js
+++ front/src/config.js
@@ -1,5 +1,5 @@
 const BaseURL =
   process.env.NODE_ENV === "production"
-    ? "https://ange-chat.onrender.com"
+    ? "https://api-ange-chat.onrender.com"
     : "http://localhost:8080";
 export default BaseURL;
